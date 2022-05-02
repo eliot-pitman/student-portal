9b1559dd64ab25cdf2ae67f7fb1a81b7fde9a03b
Eliot <eliotpitman@gmail.com>
2022-05-02 13:56:26 -0500

    resolves conflict

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-<<<<<<< HEAD
+
     <h1>This is Eliot's comment!!!!!</h1>
-=======
+
     <p>I put stuff here</p>
->>>>>>> 00d27592d601a76c8dad7f0a83d6f4c369e6f85f
   </div>
 </template>
 
