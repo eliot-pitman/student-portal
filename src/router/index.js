99f2e24411f293e00c2b9639ed9994c79f29254c
Will Grund <willgrund178@gmail.com>
2022-05-03 15:25:57 -0400

    fixes student show router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import LogIn from "../views/LogIn.vue";
 import StudentsIndex from "../views/StudentsIndex.vue";
 import LogOut from "../views/LogOut.vue";
+import StudentShow from "../views/StudentShow.vue";
 
 const routes = [
   {
