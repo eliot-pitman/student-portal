7eace74558631cbca6f6ce4c17a96eeb6d200173
John Grieve <jonathanrgrieve@gmail.com>
2022-05-03 12:01:54 -0500

    adds index route

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
     <router-link to="/about">About</router-link>
     |
     <router-link to="/login">Login</router-link>
+    |
+    <router-link to="/Index">Index</router-link>
   </nav>
   <router-view />
 </template>
