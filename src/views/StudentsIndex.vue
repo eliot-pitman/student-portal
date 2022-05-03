b36b54269617fb35c4f69e33e484e2217f52d145
Will Grund <willgrund178@gmail.com>
2022-05-03 12:42:55 -0400

    adds axios get request

--- src/views/StudentsIndex.vue
+++ src/views/StudentsIndex.vue
@@ -1,7 +1,17 @@
 <script>
+import axios from "axios";
 export default {
   data: function () {
-    return {};
+    return {
+      data: [],
+    };
+  },
+  created: function () {
+    //  update get request url when we get route from backend people
+    axios.get("http://localhost:3000/something.json").then((response) => {
+      this.data = response.data;
+      console.log("", this.data);
+    });
   },
 };
 </script>
