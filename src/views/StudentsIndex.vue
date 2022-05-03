ec935f286716461ae5d6f00c71782e806336fa49
jason <jduong89@gmail.com>
2022-05-03 11:45:00 -0700

    adds destroy function for exp,edu,skills,capstone

--- src/views/StudentsIndex.vue
+++ src/views/StudentsIndex.vue
@@ -12,6 +12,32 @@ export default {
       console.log("All Students", this.students);
     });
   },
+  methods: {
+    destroyExperience: function () {
+      axios.delete("/" + experience.id).then((response) => {
+        console.log("experience removed", response);
+        this.$router.push("/");
+      });
+    },
+    destroyEducation: function () {
+      axios.delete("/" + education.id).then((response) => {
+        console.log("education removed", response);
+        this.$router.push("/");
+      });
+    },
+    destroySkill: function () {
+      axios.delete("/" + skill.id).then((response) => {
+        console.log("skill removed", response);
+        this.$router.push("/");
+      });
+    },
+    destroyCapstone: function () {
+      axios.delete("/" + capstone.id).then((response) => {
+        console.log("capstone removed", response);
+        this.$router.push("/");
+      });
+    },
+  },
 };
 </script>
 
